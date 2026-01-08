67a1702f0e366f2c096e895a9a268b35fe74b362
Lene <Lene-tl@hotmail.com>
2026-01-08 13:49:00 +0100

    Update Program.cs

--- ParProg_Pins/Program.cs
+++ ParProg_Pins/Program.cs
@@ -2,6 +2,18 @@
 {
     internal class Program
     {
+        public List<Pin> pinList = new List<Pin>()
+        {
+            new Pin("Tønsberg", 120),
+            new Pin("Stavern", 120),
+            new Pin("Larvik", 120),
+            new Pin("Sandefjord", 120),
+            new Pin("Verdens Ende", 120),
+        };
+        public List<Bensinstasjon> besinstasjonList = new List<Bensinstasjon>()
+        {
+            new Bensinstasjon("")
+        };
         static void Main(string[] args)
         {
             
